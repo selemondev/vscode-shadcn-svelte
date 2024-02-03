7500f062a0763cb4814b6883f820b56579fc5a1b
selemondev <selemondev19@gmail.com>
2024-02-03 09:29:00 +0300

    refactor(bundler): migrate from webpack to tsup

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+    entry: ['./src/extension.ts'],
+    outDir: "dist",
+    dts: false,
+    splitting: false,
+    clean: true,
+    platform: 'node',
+    sourcemap: false,
+    external: ['vscode']
+})
